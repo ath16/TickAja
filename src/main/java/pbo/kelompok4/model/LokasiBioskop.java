6d864630c1e283003902b0f454d623646bf933ac
lopie25 <wellysimarmata338@gmail.com>
2026-01-06 15:23:04 +0800

    LokasiBioskop

--- src/main/java/pbo/kelompok4/model/LokasiBioskop.java
+++ src/main/java/pbo/kelompok4/model/LokasiBioskop.java
@@ -2,4 +2,31 @@ package pbo.kelompok4.model;
 
 public class LokasiBioskop {
     
+    private int idLokasi;
+    private String namaLokasi;
+    private String alamat;
+
+    public LokasiBioskop(int idLokasi, String namaLokasi, String alamat){
+        this.idLokasi = idLokasi;
+        this.namaLokasi = namaLokasi;
+        this.alamat = alamat;
+    }
+
+    public LokasiBioskop(String namaLokasi, String alamat) {
+        this.namaLokasi = namaLokasi;
+        this.alamat = alamat;
+    }
+
+    public int getIdLokasi(){
+        return idLokasi;
+    }
+
+    public String getNamaLokasi(){
+        return namaLokasi;
+    }
+
+    public String getAlamat(){
+        return alamat;
+    }
+
 }
--- src/main/java/pbo/kelompok4/model/Studio.java
+++ src/main/java/pbo/kelompok4/model/Studio.java
@@ -1,5 +1,30 @@
 package pbo.kelompok4.model;
 
 public class Studio {
+    private int idStudio;
+    private int idLokasi;
+    private String namaStudio;
+
+    public Studio(int idStudio, int idLokasi, String namaStudio){
+        this.idStudio = idStudio;
+        this.idLokasi = idLokasi;
+        this.namaStudio = namaStudio;
+    }
+
+    public Studio(int idLokasithis, String namaStudio){
+        this.idLokasi = idLokasi;
+        this.namaStudio = namaStudio;
+    }
     
+    public int getIdStudio(){
+        return idStudio;
+    }
+
+    public int getIdLokasi(){
+        return idLokasi;
+    }
+
+    public String getNamaStudio(){
+        return namaStudio;
+    }
 }
